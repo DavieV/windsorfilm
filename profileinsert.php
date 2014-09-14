f6309362bb1d2543047dd41bbe4209132d3bb4b2
David Valleau <davidvalleau@gmail.com>
2014-09-14 18:10:47 -0400

    fixed profile insert

--- profileinsert.php
+++ profileinsert.php
@@ -43,31 +43,31 @@ if($membership == 1){
 		}
 		header("location: index.php");
 	}
-	header("location: index.php");
+	echo "you goofed";
 }
 
 else if($membership == 2){
 	$talents = array($_POST['talent1'], $_POST['talent2'], $_POST['talent3'], $_POST['talent4'], $_POST['talent5']);
 	if(validTalents($talents, $jobsArray)){
-		if($stmt=$mysqli->prepare("UPDATE test SET phone=?, businessphone=?, image=?, bio=?, talent1=?, talent2=?, talent3=?, talent4=?, talent5=? WHERE username=?")){
+		if($stmt=$mysqli->prepare("UPDATE test SET phone=?, businessphone=?, image=?, bio=?, talent1=?, talent2=?, talent3=?, talent4=?, talent5=? WHERE id=?")){
 			$stmt->bind_param("sssssssssd", $phone, $businessphone, $image, $bio, $talents[0], $talents[1], $talents[2], $talents[3], $talents[4], $id);
 			$stmt->execute();
 			$stmt->close();
 		}
 		header("location: index.php");
 	}
-	header("location: index.php");
+	echo "you goofed";
 }
 
 else if($membership == 3){
 	$talents = array($_POST['talent1'], $_POST['talent2'], $_POST['talent3'], $_POST['talent4'], $_POST['talent5'], $_POST['talent6'], $_POST['talent7']);
 	if(validTalents($talents, $jobsArray)){
-		if($stmt=$mysqli->prepare("UPDATE test SET phone=?, businessphone=?, image=?, video=?, bio=?, talent1=?, talent2=?, talent3=?, talent4=?, talent5=?, talent6=?, talent7=? WHERE username=?")){
+		if($stmt=$mysqli->prepare("UPDATE test SET phone=?, businessphone=?, image=?, video=?, bio=?, talent1=?, talent2=?, talent3=?, talent4=?, talent5=?, talent6=?, talent7=? WHERE id=?")){
 			$stmt->bind_param("ssssssssssssd", $phone, $businessphone, $image, $video, $bio, $talents[0], $talents[1], $talents[2], $talents[3], $talents[4], $talents[5], $talents[6], $id);
 			$stmt->execute();
 			$stmt->close();
 		}
 		header("location: index.php");
 	}
-	header("location: index.php");
+	echo "you goofed";
 }
