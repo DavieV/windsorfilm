91b93907b5bef15053b369a774d5b4cf95cca11b
Quinny <thequinnftw@gmail.com>
2014-09-15 14:47:59 +0000

    user object changes merged

--- includes/video.php
+++
@@ -1,7 +0,0 @@
-<div class="col-md-12 col-xl-8 col-xl-offset-2">
-	<div class="embed-responsive embed-responsive-16by9">
-		<?php
-		echo preg_replace("/\s*[a-zA-Z\/\/:\.]*youtube.com\/watch\?v=([a-zA-Z0-9\-_]+)([a-zA-Z0-9\/\*\-\_\?\&\;\%\=\.]*)/i","<iframe class='embed-responsive-item' src=\"//www.youtube.com/embed/$1\"></iframe>", $info[6]);
-		?>
-	</div>
-</div>
